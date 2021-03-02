dee4196f4065124e4d833615769bcf27d050a45a
Zhangxinnian <59382173+Zhangxinnian@users.noreply.github.com>
2021-03-02 16:18:31 +0800

    Add files via upload

---
+++ Action Recognition/ECO-caffe/models_ECO_Full/hmdb51/run.sh
@@ -0,0 +1,15 @@
+
+mkdir -p snapshots
+mkdir -p training
+
+
+HDF5_DISABLE_VERSION_CHECK=1 ../caffe_3d/build/tools/caffe train --solver=solver.prototxt --weights=/models/ECO_full_kinetics.caffemodel 2>&1 | tee -a training/log.txt
+
+
+
+
+
+
+
+
+
